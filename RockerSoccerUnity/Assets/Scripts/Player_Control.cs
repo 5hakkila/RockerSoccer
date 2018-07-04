0a3b69b92a00d277187530076f75e36ab608e33b
Sami Hakkila <samihakkila@kamk.fi>
2018-07-04 11:43:46 +0300

    Revert "Merge branch 'master' of https://github.com/SamiHakkila/RockerSoccer"
    This reverts commit 4414d79c90fcae7e8d14539d9cb0f75d1ee15f20, reversing
    changes made to 035ce3458a6a096c28afe5696b7832db3f8ab034.

--- RockerSoccerUnity/Assets/Scripts/Player_Control.cs
+++ RockerSoccerUnity/Assets/Scripts/Player_Control.cs
@@ -25,9 +25,8 @@ public class Player_Control : MonoBehaviour {
 
     private  float timer = 0.0f;
    
-    private float jumpForce =400.0f;
-    
-    private bool canKick = false;
+    private float jumpForce =0.0f;
+    public bool canKick = false;
     private float kickForce = 200.0f;
 
     bool facingRight = true;
@@ -37,15 +36,9 @@ public class Player_Control : MonoBehaviour {
     Vector2 direction;
     public float getDistance;
     public bool isBallPlayable = false;
-    // public GameObject getInput;
+   // public GameObject getInput;
 
-    //  public bool canLaunch = true;
-    //Ground check variables
-    public Transform groundCheck;
-    float groundRadius = 0.1f;
-    public LayerMask whatIsGround;
-    public bool grounded = false;
-
+  //  public bool canLaunch = true;
     // Use this for initialization
     void Start () {
         player_state = PlayerState.IDLE;
@@ -65,7 +58,7 @@ public class Player_Control : MonoBehaviour {
         
         //ignore collision between player and ball
         Physics2D.IgnoreLayerCollision(8, 9, true);
-        Physics2D.IgnoreLayerCollision(8, 8, true);
+
         /*
         if (TouchControl.inputState == TouchControl.InputState.LowerRight || TouchControl.inputState == TouchControl.InputState.LowerLeft)
             { 
@@ -86,15 +79,12 @@ public class Player_Control : MonoBehaviour {
            
         }
 
-
-
-
-
+       
 
         if (active == true)
         {
             Debug.Log(TouchControl.inputState);
-
+           
             // getDistance between player and ball
             getDistance = GameObject.Find("Player_Manager").GetComponent<Player_Manager>().sendShortest;
 
@@ -105,113 +95,77 @@ public class Player_Control : MonoBehaviour {
             direction = endDirection - startDirection;
             direction.Normalize();
 
+            //  transform.position = Vector2.MoveTowards(player.transform.position, ball.position,2.0f *Time.deltaTime);
 
-            //move player right
-            if (player.transform.position.x < ball.transform.position.x && getDistance > 0.8f && grounded == true)
+            if (player.transform.position.x < ball.transform.position.x && getDistance > 0.8f)
             {
                 player_state = PlayerState.RIGHT;
                 // playerR.velocity = new Vector2(4.0f, 0.0f);
                 playerR.velocity = new Vector2(4.0f, playerR.velocity.y);
 
             }
-            if (player.transform.position.x < ball.transform.position.x && getDistance > 0.8f && grounded == false)
-            {
-                player_state = PlayerState.RIGHT;
-                // playerR.velocity = new Vector2(4.0f, 0.0f);
-                playerR.velocity = new Vector2(0.0f, playerR.velocity.y);
-
-            }
-
-
-
-            // move player left
-            if (player.transform.position.x > ball.transform.position.x && getDistance > 0.8f && grounded == true)
+            if (player.transform.position.x > ball.transform.position.x && getDistance > 0.8f)
             {
                 player_state = PlayerState.LEFT;
                 //    playerR.velocity = new Vector2(-4.0f, 0.0f);
                 playerR.velocity = new Vector2(-4.0f, playerR.velocity.y);
 
             }
-            if (player.transform.position.x > ball.transform.position.x && getDistance > 0.8f && grounded == false)
-            {
-                player_state = PlayerState.LEFT;
-                //    playerR.velocity = new Vector2(-4.0f, 0.0f);
-                playerR.velocity = new Vector2(0.0f, playerR.velocity.y);
 
-            }
-
-
+           
             if (TouchControl.inputState == TouchControl.InputState.Down)
             {
-
-
-            }
-
-            //jump
-            if (TouchControl.inputState == TouchControl.InputState.Up && canKick == false && isBallPlayable == true && grounded == true)
-            {
-                playerR.velocity = new Vector2(0, 0);
-                playerR.AddForce(new Vector2(0, jumpForce));
+                
 
             }
 
-            //kick ball start*******************************************************************************************************************
-            if (TouchControl.inputState == TouchControl.InputState.UpperRight && canKick == true && isBallPlayable == true && grounded == true)
+            if (TouchControl.inputState == TouchControl.InputState.UpperRight && canKick ==true && isBallPlayable == true)
             {
 
-
+                // ballR.AddForce(direction * 1.0f * 200.0f);           
                 ballR.velocity = new Vector2(0.0f, 0.0f);
                 ballR.AddForce(direction * 1.0f * kickForce);
             }
 
-
-            if (TouchControl.inputState == TouchControl.InputState.Up && canKick == true && isBallPlayable == true && grounded == true)
+            
+            if (TouchControl.inputState == TouchControl.InputState.Up && canKick == true && isBallPlayable == true)
             {
                 ballR.velocity = new Vector2(0.0f, 0.0f);
                 ballR.AddForce(direction * 1.0f * kickForce);
+                // ballR.AddForce(direction * 1.0f);
+                //  playerR.AddForce(new Vector2(0.0f, 600.0f));
+                // ballR.AddForce(direction * 1.0f  * 200.0f);     
 
+               
             }
 
 
-            if (TouchControl.inputState == TouchControl.InputState.UpperLeft && canKick == true && isBallPlayable == true && grounded == true)
+            if (TouchControl.inputState == TouchControl.InputState.UpperLeft && canKick == true && isBallPlayable == true)
             {
                 ballR.velocity = new Vector2(0.0f, 0.0f);
                 ballR.AddForce(direction * 1.0f * kickForce);
+                // ballR.AddForce(direction * 1.0f  * 200.0f);    
 
             }
-
-            if (TouchControl.inputState == TouchControl.InputState.Left && canKick == true && isBallPlayable == true && grounded == true)
+            if (TouchControl.inputState == TouchControl.InputState.Left && canKick == true && isBallPlayable == true)
             {
                 ballR.velocity = new Vector2(0.0f, 0.0f);
                 ballR.AddForce(direction * 1.0f * kickForce);
-
+                // ballR.AddForce(direction * 1.0f  * 200.0f);    
 
             }
-
-            if (TouchControl.inputState == TouchControl.InputState.Right && canKick == true && isBallPlayable == true && grounded == true)
+            if (TouchControl.inputState == TouchControl.InputState.Right && canKick == true && isBallPlayable == true)
             {
                 ballR.velocity = new Vector2(0.0f, 0.0f);
                 ballR.AddForce(direction * 1.0f * kickForce);
-
-            }
-            // kick ball end *******************************************************************************************************************
-
-            //on air kick header
-            if (canKick == true && isBallPlayable == true && grounded == false)
-            {
-                ballR.velocity = new Vector2(0.0f, 0.0f);
-                ballR.AddForce(direction * 1.0f * (kickForce / 2));
-
+                // ballR.AddForce(direction * 1.0f  * 200.0f);    
 
             }
         }
-        // move back to staring position
-        if (active == false && grounded ==true)
-        {
+        else
             transform.position = Vector2.MoveTowards(player.transform.position, start_position.position, 0.1f);
-        }
 
-        //flip player direction
+
         if (playerR.velocity.x > 0 && !facingRight && Mathf.Abs(player.transform.position.x - ball.transform.position.x) > 0.1f)
         {
 
@@ -227,11 +181,6 @@ public class Player_Control : MonoBehaviour {
 
 
 }
-    private void FixedUpdate()
-    {
-        //Ground check
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-    }
     void Flip()
     {
         facingRight = !facingRight;
