1502741173cdc4c014a55d3c986d3dcd1aca4e8f
Sami Hakkila <samihakkila@kamk.fi>
2018-07-16 21:41:00 +0300

    rockerSoccer0.8

---
+++ 15_7_2018_3d/RockerSoccerUnity_2/Assets/Scripts/Ball_material.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_material : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject ball;
+    public Material ball_material;
+
+    public Material holomaterial ;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+        ball.GetComponent<MeshRenderer>().material = ball_material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "player" )
+        {
+
+            ball.GetComponent<MeshRenderer>().material = holomaterial;
+           // print("enter");
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "player" )
+        {
+
+            ball.GetComponent<MeshRenderer>().material = ball_material;
+
+           // print("exit");
+        }
+    }
+
+}
---
+++ 15_7_2018_3d/RockerSoccerUnity_2/Assets/Scripts/Ball_rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_rotate : MonoBehaviour {
+    public Rigidbody2D ball;
+    private float horizontalSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        horizontalSpeed = ball.velocity.x;
+
+
+        if (horizontalSpeed != 0.0f)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * (horizontalSpeed * -20.0f), Space.World);
+        }
+    }
+}
